ceb914f4a95e89dfd60397fdbd2b9f52aece978f
Jean Carlos de Conto Filho <filhojean316@gmail.com>
2025-05-20 18:54:27 -0300

    Estrutura para consumir a api pronta

--- InvestTrack.js
+++ InvestTrack.js
@@ -1,35 +1,37 @@
+const ApiAlta = "https://investtrack-j5re.onrender.com/acoes/altas"
+const ApiBaixa = "https://investtrack-j5re.onrender.com/acoes/baixas"
+const ApiPopular = "https://investtrack-j5re.onrender.com/acoes/populares"
+
+// chrome.exe --disable-web-security --user-data-dir="C:/temp-chrome"
+
 document.addEventListener("DOMContentLoaded", () => {
-    const ApiAlta = "https://investtrack-j5re.onrender.com/acoes/altas"
-    const ApiBaixa = "https://investtrack-j5re.onrender.com/acoes/baixas"
-    const ApiPopular = "https://investtrack-j5re.onrender.com/acoes/populares"
-    
 
     function Alta() {
         fetch(ApiAlta)
             .then((res) => res.json())
-            .then(result => {
-                result.forEach(alta => {
+            .then(data => console.log("Alta",data));
 
-                });
-            })
     }
+    Alta()
 
     function Baixa() {
         fetch(ApiBaixa)
             .then((res) => res.json())
-            .then(result => {
-                result.forEach(baixa => {
+            .then(data1 => console.log("Baixa", data1));
 
-                });
-            })
     }
+    Baixa()
     function Popular() {
         fetch(ApiPopular)
             .then((res) => res.json())
-            .then(result => {
-                result.forEach(popular => {
+            .then(data2 => console.log("Popular",data2));
 
-                });
-            })
     }
+    Popular()
+
+
+
+
+
+    
 })
