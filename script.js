df314460c753f90fba947162a9240f5cad963942
Lay-santos <Layane_santos14@outlook.com>
2025-05-21 20:16:21 -0300

    atualizando o grfáfico e adicionando mais telas

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 const ctx = document.getElementById('stockChart').getContext('2d');
- 
+
 const stockChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: ['09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00'],
+    labels: ['10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00'],
     datasets: [{
       label: 'Preço',
-      data: [5.1050, 5.1070, 5.1100, 5.1080, 5.1090, 5.1065, 5.1083],
+      data: [1.5, 5.1070, 5.1100, 5.1080, 10, 5.1065, 8, 5],
       borderColor: '#58a6ff',
-      backgroundColor: 'rgba(255, 255, 255, 0.1)',
+      backgroundColor: 'rgba(255, 255, 255, 0)',
       fill: true,
       tension: 0.3,
-      pointRadius: 0,
+      pointRadius: 3,
+      pointHoverRadius: 5
+      
     }]
   },
   options: {
@@ -19,11 +21,18 @@ const stockChart = new Chart(ctx, {
     plugins: {
       legend: {
         display: false
-      },
+      }
+    },
+    hover: {
+      intersect: false
+    },
+    interaction: {
+      mode: 'index', 
+      intersect: false
     },
     scales: {
       x: {
-        display: false
+        display: true
       },
       y: {
         display: false
