b3039a86699cb32abd63730989673a410804d185
Jean Carlos de Conto Filho <filhojean316@gmail.com>
2025-05-21 21:26:54 -0300

    todas as colunas populadas e html e css modificados

--- InvestTrack.js
+++ InvestTrack.js
@@ -1,8 +1,11 @@
-let board = document.querySelector(".board")
+let boardAltas = document.querySelector(".boardAltas")
+let boardBaixas = document.querySelector(".boardBaixas")
+let boardPopulares = document.querySelector(".boardPopulares")
+
 
 let cardPopulares = document.querySelector(".cardPopulares")
 let cardAltas = document.querySelector(".cardAltas")
-let CardBaixas = document.querySelector(".CardBaixas")
+let cardBaixas = document.querySelector(".cardBaixas")
 
 let contentPopulares = document.querySelector(".contentPopulares")
 let contentAltas = document.querySelector(".contentAltas")
@@ -24,15 +27,17 @@ document.addEventListener("DOMContentLoaded", () => {
                     const divAlta = document.createElement("div")
 
                     divAlta.classList.add("cardAltas")
-                    divAlta.innerHTML = 
-                    `
+                    divAlta.innerHTML =
+                        `
                     <div class="contentAltas">
                             <p>${element.name}</p>
+                            <p>${element.stock}</p>
+                            <p>${element.close}</p>
                     </div>
                     
 
                     `
-                    board.append(divAlta)
+                    boardAltas.append(divAlta)
                 });
             });
 
@@ -42,15 +47,46 @@ document.addEventListener("DOMContentLoaded", () => {
     function Baixa() {
         fetch(ApiBaixa)
             .then((res) => res.json())
-            .then(data1 => console.log("Baixa", data1));
+            .then(data1 => {
+                data1.stocks.forEach(element => {
+                    const divBaixa = document.createElement("div")
+
+                    divBaixa.classList.add("cardBaixas")
+                    divBaixa.innerHTML =
+                    `
+                    <div class="contentBaixas">
+                            <p>${element.name}</p>
+                            <p>${element.stock}</p>
+                            <p>${element.close}</p>
+                    </div>
+                `
+                    boardBaixas.append(divBaixa)
+
+                })
+            });
 
     }
     Baixa()
     function Popular() {
         fetch(ApiPopular)
             .then((res) => res.json())
-            .then(data2 => console.log("Popular", data2));
+            .then(data2 => {
+                data2.stocks.forEach(element => {
+                    const divPopulares = document.createElement("div")
 
+                    divPopulares.classList.add("cardBaixas")
+                    divPopulares.innerHTML =
+                    `
+                    <div class="contentBaixas">
+                            <p>${element.name}</p>
+                            <p>${element.stock}</p>
+                            <p>${element.close}</p>
+                    </div>
+                `
+                    boardPopulares.append(divPopulares)
+
+                })
+            });
     }
     Popular()
 
