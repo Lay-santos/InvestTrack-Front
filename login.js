4d864671a409d8efd9217942db4b7451dc2578d0
Jean Carlos de Conto Filho <filhojean316@gmail.com>
2025-05-30 21:16:45 -0300

    integração da tela de login feita

--- login.js
+++ login.js
@@ -1,34 +1,44 @@
+const InputEmail = document.getElementById("email")
+const InputPassword = document.getElementById("password")
 const formLogin = document.getElementById("formLogin")
+
 const loginUser = "http://localhost:8080/user/login"
 // token é apenas usado para os favoritos e toda vez que se faz um login é criado um token temporário 
 
 formLogin.addEventListener("submit", async function (event) {
     event.preventDefault()
 
-    const InputEmail = document.getElementById("email").value
-    const InputPassword = document.getElementById("password").value
+    const login = {
+        email: InputEmail.value,
+        password: InputPassword.value
+    }
 
-    if (InputEmail == ""|| InputPassword=="") {
+
+    if (InputEmail == "" || InputPassword == "") {
         alert("Todos os campos precisão ser preenchidos")
-        return
-    } 
-
-    const res = await fetch(loginUser,{
-        method:"POST",
-        headers:{"Content-Type": "application/json"},
-        body: JSON.stringify({InputEmail, InputPassword})
-    })
-
-     if (!res.ok) {
-        alert("Erro ao efetuar o login")
-        console.log(Error);
-        
         return
     }
 
-    const data = await res.json();
-    sessionStorage.setItem("tokenInvestTrack", data.token)
-    alert("O login foi realizado com sucesso")
+    const res = await fetch(loginUser, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(login)
+    })
 
+    console.log(res)
+
+    if (!res.ok) {
+        alert("Erro ao efetuar o login")
+        console.log(Error);
+
+    } else {
+
+        const data = await res.text();
+        console.log(data)
+        sessionStorage.setItem("tokenInvestTrack", data)
+        alert("O login foi realizado com sucesso")
+        window.location.href = "index.html"
+
+    }
 
 })
